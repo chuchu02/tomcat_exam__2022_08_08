3387922cd30c785042d229b39458670e5c51b16c
chuchu02 <rlaxogns73@gmail.com>
2022-08-09 00:16:38 +0900

    구구단 서블릿 추가

--- src/main/java/com/ll/exam/HelloServlet.java
+++ src/main/java/com/ll/exam/HelloServlet.java
@@ -17,6 +17,6 @@ public class HelloServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
         resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
 
-        resp.getWriter().append("크하하하하하!!!");
+        resp.getWriter().append("크하하!!! 쿠쿠쿸쿠");
     }
 }
